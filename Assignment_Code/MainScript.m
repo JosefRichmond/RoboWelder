a3e499f6ee837aaffc2e7ca5db80466f2e2aa833
Alistair Higgins <alistairjohnhiggins@gmail.com>
2021-05-07 12:45:04 +1000

    message: add logger file, ply files for models, enviro class and placeholder ur3 class

--- Assignment_Code/MainScript.m
+++ Assignment_Code/MainScript.m
@@ -3,3 +3,25 @@
 %               Reece Holmewood 12875629
 %               Josef Richmond 12875860
 % Main 
+
+clc; 
+clear all;
+close all;
+
+%% Logger
+log = log4matlab(datestr(now,'ddmmyy_HH_MM_SS_AM.log'));
+log.SetCommandWindowLevel(log.DEBUG)
+
+%% Load Robot
+disp('Creating Robots')
+RoboWelder = UR3('test', transl(0.75,0,0.5), false,log);
+close all
+
+%% Load Environment
+weldEnvironment = A2Environment ([-5 5 -5 5 0 5]);
+hold on
+
+%myEnvironment.LoadEquipment
+%myEnvironment.LoadRobot(RoboWelder)
+
+%%
