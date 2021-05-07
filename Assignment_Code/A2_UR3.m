2ee3da16d81915939e1b9e9499509f0d6d4e5369
Josef Richmond <josef.richmond12@gmail.com>
2021-05-07 17:52:50 +1000

    Updated name of assignment code file

--- Assignment_Code/A2_UR3.m
+++ Assignment_Code/A2_UR3.m
@@ -1,9 +1,2 @@
 %% ASSIGNMENT #2 UR3 CLASS
-% Contributors: Alistair Higgins 12934600
-%               Reece Holmewood 12875629
-%               Josef Richmond 12875860
-% Main % Robotics 41013 Lab Assignment 2
-% Contributors: Alistair Higgins 12934600
-%               Reece Holmewood 12875629
-%               Josef Richmond 12875860
-% Main 
+%Contain properties and methods for use
--- Assignment_Code/Sensor/Demo.m
+++
@@ -1,9 +0,0 @@
-try 
-    rosinit
-end 
-mySensor = Sensor();
-try
-    s = rossubscriber('/usb_cam/image_raw/compressed',@mySensor.detectFaces, 'BufferSize',1);
-catch
-    clear('s')
-end 
